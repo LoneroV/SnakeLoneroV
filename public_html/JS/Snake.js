1f469c066cb9828d7221817f661062065b76b860
Vincent Lonero <vincent.lonero.16@gmail.com>
2014-10-06 12:11:03 -0700

    Day 2 commit

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -4,8 +4,18 @@ var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 function gameInitialize() {
+    var canvas = document.getElementbyID("game-screen");
+    context = canvas.getContext("2d");
     
+    screenWidth = window.innerWidth;
+    screenHeight = window. innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
@@ -13,5 +23,6 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillStyle = "rgb(3 , 140, 21)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
 }
